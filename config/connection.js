c6b9ad8d9c4af01fc0794a974b458f68c01e4f05
Nico Reed <nr830411@gmail.com>
2020-02-22 14:06:59 -0500

    (trying to establish connection)

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,8 @@
 var mysql = require("mysql");
+var connection; 
 
-// var connection = mysql.createConnection({
-//   host: "localhost",
-//   port: 3306,
-//   user: "root",
-//   password: "",
-//   database: "burgers_db"
-// });
 
-if (process.env,JAWSDB_URL) {
+if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
   connection = mysql.createConnection({
